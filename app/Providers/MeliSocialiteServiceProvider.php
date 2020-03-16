c69e25580ba5a2efaf4fe6b373d72bfeef3a0431
John Espindola <johnalexandere@hotmail.com>
2020-03-16 14:36:28 -0500

    auth config

--- app/Providers/MeliSocialiteServiceProvider.php
+++ app/Providers/MeliSocialiteServiceProvider.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\ServiceProvider;
 
+use Laravel\Socialite\Two\User;
+
 class MeliSocialiteServiceProvider extends ServiceProvider
 {
     /**
@@ -451,5 +453,45 @@ class MeliManager
             $uri = $uri.$params;
         }
         return $uri;
+    }
+}
+
+
+
+
+
+class MeliUser extends User
+{
+    public $refresh_token;
+
+    protected $expired_in;
+
+    public $expires_at;
+
+    /**
+     * Set the token on the user.
+     *
+     * @param  string  $refresh_token
+     * @return $this
+     */
+    public function setRefreshToken($refresh_token)
+    {
+        $this->refresh_token = $refresh_token;
+
+        return $this;
+    }
+
+    /**
+     * Set the token on the user.
+     *
+     * @param  string  $expires_in
+     * @return $this
+     */
+    public function setExpiresIn($expires_in)
+    {
+        $this->expired_in = $expires_in;
+        $this->expires_at = time()+$expires_in;
+
+        return $this;
     }
 }
