4f8c957e7aa32a93d3e6e4b763e5a50061aefddd
John Espindola <johnalexandere@hotmail.com>
2020-03-16 14:48:57 -0500

    auth config

--- app/Providers/MeliSocialiteServiceProvider.php
+++ app/Providers/MeliSocialiteServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 //namespace Kolovious\MeliSocialite;
 
+use Kolovious\MeliSocialite;
+
 use Laravel\Socialite\Two\AbstractProvider;
 use Laravel\Socialite\Two\InvalidStateException;
 use Laravel\Socialite\Two\ProviderInterface;
@@ -458,38 +460,3 @@ class MeliManager
 
 
 
-class MeliUser extends User
-{
-    public $refresh_token;
-
-    protected $expired_in;
-
-    public $expires_at;
-
-    /**
-     * Set the token on the user.
-     *
-     * @param  string  $refresh_token
-     * @return $this
-     */
-    public function setRefreshToken($refresh_token)
-    {
-        $this->refresh_token = $refresh_token;
-
-        return $this;
-    }
-
-    /**
-     * Set the token on the user.
-     *
-     * @param  string  $expires_in
-     * @return $this
-     */
-    public function setExpiresIn($expires_in)
-    {
-        $this->expired_in = $expires_in;
-        $this->expires_at = time()+$expires_in;
-
-        return $this;
-    }
-}
