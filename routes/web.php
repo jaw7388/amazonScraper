36f1e98ea8ee23deb4ef0d6bf2353055d8ac7fb6
John Espindola <johnalexandere@hotmail.com>
2020-03-17 12:30:32 -0500

    auth config

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('auth/meli', 'Auth\AuthController@redirectToProvider');
-Route::get('auth/meli/callback', 'Auth\AuthController@handleProviderCallback');
+Route::get('auth/meli', 'Auth\LoginController@redirectToProvider');
+Route::get('auth/meli/callback', 'Auth\LoginController@handleProviderCallback');
