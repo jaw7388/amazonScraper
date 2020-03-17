f708eec6f1a96691a7c8bfef85059ffa17f34698
John Espindola <johnalexandere@hotmail.com>
2020-03-17 12:29:00 -0500

    auth config

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,35 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+
+    /**
+     * Redirect the user to the Meli authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('meli')->redirect();
+    }
+
+    /**
+     * Obtain the user information from Meli.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $meliUser = Socialite::driver('meli')->user();
+
+        //dd($meliUser);
+
+        Auth::login($meliUser, true);
+
+        return redirect($this->redirectTo);
+
+    }  //
+
+    
 }
