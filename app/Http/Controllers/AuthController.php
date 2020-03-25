438e0eb63e0cbf717c1d5cbbf75856a9326b04bf
John Espindola <johnalexandere@hotmail.com>
2020-03-25 16:22:25 -0500

    auth config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
   /**
      * Redirect the user to the Meli authentication page.
      *
