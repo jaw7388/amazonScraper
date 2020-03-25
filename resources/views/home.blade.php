d39048e4ae83b2fb46974caaba2361dd75bcaac3
John Espindola <johnalexandere@hotmail.com>
2020-03-25 14:35:49 -0500

    auth config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,12 @@
     {{ Auth::user()->id ?? '' }}
 
 @php
-      $params = array('access_token' => $access_token);
-$result = $meli->get('/users/me', $params, true); 
+        //$result = Meli::withToken();    
+        
+        //$access_token = User::where('id', Auth::user()->id )->firstOrFail();
+        //$params = array('access_token' => $access_token);
+        $result = Meli::withToken()->get('/users/me', $params, true); 
+        print_r($result);
 @endphp
 
 @endsection
