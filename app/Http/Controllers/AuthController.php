65ef3b9a3a310705a9ea87585d78a4ab40c364ba
John Espindola <johnalexandere@hotmail.com>
2020-03-28 13:47:51 -0500

    Admilte config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,7 +57,7 @@ class AuthController extends Controller
                 'ml_avatar' => $meliUser->avatar,
                 'expires_at' => $meliUser->expires_at],  
         );
-        $avatar = $meliUser->user->thumbnail;
+        $avatar = $meliUser->user;
         return view('home', [ 'avatar'=>$avatar]);
         // $mlUser['expires_at'] = $meliUser->expires_at;
         // $mlUser['token'] = $meliUser->token;
