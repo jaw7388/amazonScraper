27bc67dcab6996d0b4417e9f6a03b28dcdc9db8c
John Espindola <johnalexandere@hotmail.com>
2020-03-28 12:12:30 -0500

    Admilte config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Events\Dispatcher;
-use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
-
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,35 +25,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(Dispatcher $events)
+    public function boot(UrlGenerator $url)
     {
-        //UrlGenerator $url
-        //Allows load CSS styles in SSL
-        // if(env('REDIRECT_HTTPS')) {
-        //     $url->formatScheme('https');
-        // }
-        if(config('app.env') === 'production') {
-//            \URL::forceScheme('https');
+        
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
         }
-
-
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            // $event->menu->addAfter('blog', [
-            //     'key' => 'account_settings',
-            //     'text' => 'Account Settings',
-            //     'topnav' => true,
-            // ]);
-            // $event->menu->addIn('account_settings', [
-            //     'key' => 'account_settings_notifications',
-            //     'text' => 'Notifications',
-            //     'url' => 'account/edit/notifications',
-            // ]);
-            // $event->menu->addBefore('account_settings_notifications', [
-            //     'key' => 'account_settings_profile',
-            //     'text' => 'Profile',
-            //     'url' => 'account/edit/profile',
-            // ]);
-        });
 
     }
 }
