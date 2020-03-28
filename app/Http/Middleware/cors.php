7fb9aa28dee3111af7881ac9f3cc76ceb461c4b5
John Espindola <johnalexandere@hotmail.com>
2020-03-28 13:57:38 -0500

    Admilte config

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -11,8 +11,9 @@ public function handle($request, Closure $next)
         //ALLOW OPTIONS METHOD
         $headers = [
             'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'Access-Control-Allow-Headers' => 'Origin, Content-Type, X-Auth-Token, Authorization, X-Requested-With, x-xsrf-token, id',
         ];
+        
         if ($request->getMethod() == "OPTIONS"){
             //The client-side application can set only headers allowed in Access-Control-Allow-Headers
             return response()->json('OK',200,$headers);
