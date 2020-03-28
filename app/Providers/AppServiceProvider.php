305eb198a91c9f2605185219fecdf584f2af7630
John Espindola <johnalexandere@hotmail.com>
2020-03-28 12:15:39 -0500

    Admilte config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,11 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        
         if(env('APP_ENV') !== 'local')
         {
             $url->forceSchema('https');
         }
-
     }
 }
